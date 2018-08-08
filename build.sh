4ce86924c99d6f3b7396caee9a32156d81fc70a1
yushijinhun <yushijinhun@gmail.com>
2018-08-08 17:54:06 +0800

    update dependencies

--- build.sh
+++ build.sh
@@ -2,22 +2,24 @@
 set -e
 shopt -s dotglob
 
-# Update local repository
-if [ ! -d upstream ]; then
-	git --bare init upstream
-	git --git-dir=upstream remote add origin "https://github.com/printempw/blessing-skin-server"
+if [[ "$SKIP_UPDATE" != "true" ]]; then
+	# Update local repository
+	if [ ! -d upstream ]; then
+		git --bare init upstream
+		git --git-dir=upstream remote add origin "https://github.com/printempw/blessing-skin-server"
+	fi
+	git --git-dir=upstream fetch --prune --prune-tags --tags origin "+refs/heads/*:refs/remotes/origin/*"
 fi
-git --git-dir=upstream fetch --prune --prune-tags --tags origin "+refs/heads/*:refs/remotes/origin/*"
 
 revision=$(<revision)
-image="blessing-skin-server:$revision"
+image="yushijinhun/blessing-skin-server:$revision"
 
 rm -rf build
-mkdir -p build
-git --git-dir=upstream --work-tree=build checkout "$revision" -- .
+mkdir -p build/src
+git --git-dir=upstream --work-tree=build/src checkout "$revision" -- .
 
 cp -r src/* build/
-sudo docker build --pull -t "$image" build
+sudo docker build --pull -t "$image" $DOCKER_OPTS build
 
 mkdir -p images
 image_save="images/$image.tar.xz"
